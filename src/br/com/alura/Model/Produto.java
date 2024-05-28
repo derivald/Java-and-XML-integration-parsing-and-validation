66c8186d64064f3f6a45c81d12d22d5f70baa9b2
Derivaldo Brito <derivaldobrito@yahoo.com.br>
2024-05-28 18:14:02 -0300

    Working with events and SAX

--- src/br/com/alura/Model/Produto.java
+++ src/br/com/alura/Model/Produto.java
@@ -10,17 +10,29 @@ public class Produto {
 		this.preco = preco;
 	}
 
+	public Produto() {
+	}
+
 	public String getNome() {
 		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public double getPreco() {
 		return preco;
 	}
-	
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
 	@Override
 	public String toString() {
-		return " Nome:" + nome + "\n Preço:"+preco+"\n";
+		return "nome:" + nome + '\n' + " Preco:" + preco;
+
 	}
 
 }
