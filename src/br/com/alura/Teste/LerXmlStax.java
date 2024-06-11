bd162b7de3e6cd69f488e40ef523d541692860cf
Derivaldo Brito <derivaldobrito@yahoo.com.br>
2024-06-11 12:08:05 -0300

    Efficient Queries with XPath

--- src/br/com/alura/Teste/LerXmlStax.java
+++ src/br/com/alura/Teste/LerXmlStax.java
@@ -19,44 +19,38 @@ public class LerXmlStax {
 		XMLInputFactory factory = XMLInputFactory.newInstance();
 		XMLEventReader eventos = factory.createXMLEventReader(is);
 		List<Produto> produtos = new ArrayList<>();
-
+		
 		while (eventos.hasNext()) {
-			
 			XMLEvent evento = eventos.nextEvent();
-			
-
-			if (evento.isStartElement()) {
-				Produto produto = CriaUmProduto(eventos);
+			if (evento.isStartElement() && evento.asStartElement().getName().getLocalPart().equals("produto")) {
+				Produto produto = criaUmProduto(eventos);
 				produtos.add(produto);
 			} 
 		}
-
 		System.out.println(produtos);
+
 	}
 
-	private static Produto CriaUmProduto(XMLEventReader eventos) throws Exception {
+	private static Produto criaUmProduto(XMLEventReader eventos) throws Exception {
 		Produto produto = new Produto();
 		
 		while(eventos.hasNext()) {
 			XMLEvent evento = eventos.nextEvent();
 			
-			if (evento.isStartDocument()) {
+			if (evento.isStartElement() && evento.asStartElement().getName().getLocalPart().equals("nome")) {
 				evento = eventos.nextEvent();
 				String nome = evento.asCharacters().getData();
 				produto.setNome(nome);
-			} else if (evento.isStartDocument()) {
+			} else if (evento.isStartElement() && evento.asStartElement().getName().getLocalPart().equals("preco")) {
 				evento = eventos.nextEvent();
 				String conteudo = evento.asCharacters().getData();
 				double preco = Double.parseDouble(conteudo);
 				produto.setPreco(preco);
-
-			} else if (evento.isEndElement() && !evento.asEndElement().getName().getLocalPart().isEmpty() ) {
-				
+			} else if (evento.isEndElement() && evento.asEndElement().getName().getLocalPart().equals("produto")) {
 				break;
-				
 			}
 		}
 		return produto;
-		
 	}
+
 }
